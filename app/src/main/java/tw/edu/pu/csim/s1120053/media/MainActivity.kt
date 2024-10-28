d1cb2fe3bc04294c30e5abe1cfce7dee90201a18
hhc-a <s1120053@o365st.pu.edu.tw>
2024-10-28 17:00:05 +0800

    3rd commit

--- app/src/main/java/tw/edu/pu/csim/s1120053/media/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1120053/media/MainActivity.kt
@@ -1,21 +1,27 @@
 package tw.edu.pu.csim.s1120053.media
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tw.edu.pu.csim.s1120053.media.ui.theme.MediaTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,15 +48,47 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         R.drawable.animal4, R.drawable.animal5,
         R.drawable.animal6, R.drawable.animal7,
         R.drawable.animal8, R.drawable.animal9)
+    var AnimalsName = arrayListOf("鴨子","企鵝","青蛙","貓頭鷹","海豚", "牛",
+        "無尾熊", "獅子", "狐狸", "小雞")
+    val context = LocalContext.current
+    var mper = MediaPlayer()
     Column {
+        Row {
+            Button(onClick = {
+                mper.reset()
+                mper = MediaPlayer.create(context, R.raw.tcyang)
+                mper.start()
+            }) {
+                Text(text = "歡迎修課", modifier=modifier)
+                Image(
+                    painter=painterResource(id=R.drawable.teacher),
+                    contentDescription="teacher",
+                    Modifier.size(50.dp)
+                )
+            }
+            Button(onClick = {
+                mper.reset()
+                mper = MediaPlayer.create(context, R.raw.fly)
+                mper.start()
+            }) {
+                Text(text = "展翅飛翔")
+                Image(
+                    painter=painterResource(id=R.drawable.fly),
+                    contentDescription="fly",
+                    Modifier.size(50.dp))
+            }
+        }
         LazyRow {
-            items(10) { index ->
+            items(20) { index ->
                 Text(index.toString(),
                 modifier=modifier)
+                Text(AnimalsName[index%10],
+                    modifier=modifier)
                 Image(
-                    painter=painterResource(id=Animals[index]),
+                    painter=painterResource(id=Animals[index%10]),
                     contentDescription="animal",
-                    modifier=modifier
+                    modifier = Modifier.fillParentMaxWidth(0.5f),
+//                    modifier = Modifier.fillParentMaxHeight(0.5f)
                 )
             }
         }
